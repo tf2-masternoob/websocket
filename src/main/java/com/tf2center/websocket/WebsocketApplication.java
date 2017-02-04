49d90609edefb3701c3c9247d1e44202a3175fa5
Chris Gunnink <chris.gunnink@topicus.nl>
2017-02-04 18:04:19 +0100

    Undertow HTTP/2

--- src/main/java/com/tf2center/websocket/WebsocketApplication.java
+++ src/main/java/com/tf2center/websocket/WebsocketApplication.java
@@ -3,6 +3,7 @@ package com.tf2center.websocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.WebSocketHandler;
@@ -10,13 +11,14 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import io.undertow.UndertowOptions;
 
 @EnableWebSocket
 @EnableScheduling
 @SpringBootApplication
 @EnableAutoConfiguration
 public class WebsocketApplication implements WebSocketConfigurer {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketApplication.class, args);
 	}
@@ -25,9 +27,16 @@ public class WebsocketApplication implements WebSocketConfigurer {
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 		registry.addHandler(webSocketHandler(), "/ws").setAllowedOrigins("*");
 	}
-	
+
 	@Bean
 	public WebSocketHandler webSocketHandler() {
 		return new TF2CenterWebsocketHandler();
+	}
+
+	@Bean
+	public UndertowEmbeddedServletContainerFactory embeddedServletContainerFactory() {
+		UndertowEmbeddedServletContainerFactory factory = new UndertowEmbeddedServletContainerFactory();
+		factory.addBuilderCustomizers(builder -> builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true));
+		return factory;
 	}
 }
